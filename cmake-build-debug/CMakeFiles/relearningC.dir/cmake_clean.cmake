eda351b1067178362845ac4d3f2502fe4ee87491
Vatsal Goel <goel01@Rajeshs-MacBook-Pro.local>
2021-09-21 15:29:52 -0400

    working on first prog

---
+++ cmake-build-debug/CMakeFiles/relearningC.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/relearningC.dir/section1/example-files/hello-world/main.c.o"
+  "relearningC"
+  "relearningC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/relearningC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
